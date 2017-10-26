a8c206f7530edad9f193060ca5a3cbfaa580173b
Christopher Jeffrey <chjjeffrey@gmail.com>
2017-10-25 18:20:39 -0700

    src: minor.

--- src/cipher.h
+++ src/cipher.h
@@ -4,22 +4,6 @@
 #define BCRYPTO_ENCIPHER_SIZE(len) ((len) + (16 - ((len) % 16)));
 #define BCRYPTO_DECIPHER_SIZE(len) (len)
 
-#ifdef BCRYPTO_USE_CIPHER
-bool
-bcrypto_cipher(
-  const char *name,
-  const uint8_t *data,
-  const uint32_t datalen,
-  const uint8_t *key,
-  const uint32_t keylen,
-  const uint8_t *iv,
-  const uint32_t ivlen,
-  uint8_t **out,
-  uint32_t *outlen,
-  const bool encrypt
-);
-#endif
-
 bool
 bcrypto_encipher(
   const uint8_t *data,
