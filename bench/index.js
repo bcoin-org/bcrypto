e787343e4ec47606f4585689d0d69b291c1ef466
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-06-15 10:49:26 -0700

    bcrypto: better benchmarks.

--- bench/index.js
+++ bench/index.js
@@ -7,32 +7,58 @@ const blake2b = require('../lib/blake2b');
 const sha3 = require('../lib/sha3');
 const random = require('../lib/random');
 const ChaCha20 = require('../lib/chacha20');
+const Poly1305 = require('../lib/poly1305');
+const AEAD = require('../lib/aead');
 
 assert.strictEqual(sha256.native, 2);
 assert.strictEqual(blake2b.native, 2);
 assert.strictEqual(sha3.native, 2);
 assert.strictEqual(ChaCha20.native, 2);
+assert.strictEqual(Poly1305.native, 2);
+assert.strictEqual(AEAD.native, 2);
 
-const rounds = 1000000;
-const msg = random.randomBytes(1024);
+for (const size of [32, 64, 65, 512, 1024]) {
+  const rounds = 2000000;
+  const msg = random.randomBytes(size);
 
-bench('sha256', rounds, () => {
-  sha256.digest(msg);
-});
+  bench(`sha256 (${size})`, rounds, () => {
+    sha256.digest(msg);
+  });
 
-bench('blake2b', rounds, () => {
-  blake2b.digest(msg);
-});
+  bench(`blake2b (${size})`, rounds, () => {
+    blake2b.digest(msg);
+  });
 
-bench('sha3', rounds, () => {
-  sha3.digest(msg);
-});
+  bench(`sha3 (${size})`, rounds, () => {
+    sha3.digest(msg);
+  });
 
-const chacha = new ChaCha20();
-const key = random.randomBytes(32);
-const iv = random.randomBytes(12);
+  const chacha = new ChaCha20();
+  const key = random.randomBytes(32);
+  const iv = random.randomBytes(12);
 
-bench('chacha20', rounds, () => {
-  chacha.init(key, iv, 0);
-  chacha.encrypt(msg);
-});
+  bench(`chacha20 (${size})`, rounds, () => {
+    chacha.init(key, iv, 0);
+    chacha.encrypt(msg);
+  });
+
+  const poly1305 = new Poly1305();
+
+  bench(`poly1305 (${size})`, rounds, () => {
+    poly1305.init(key);
+    poly1305.update(msg);
+    poly1305.final();
+  });
+
+  const aead = new AEAD();
+
+  bench(`aead (${size})`, rounds, () => {
+    aead.init(key, iv);
+    // aead.aad(ad);
+    aead.encrypt(msg);
+    aead.final();
+  });
+
+  if (size !== 1024)
+    console.log('---');
+}
