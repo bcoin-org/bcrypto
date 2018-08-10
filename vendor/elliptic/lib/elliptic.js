7520ad27dcc6bbf54a871ce639fc3b0c3d47b5e9
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-08-09 23:53:07 -0700

    pkg: vendor elliptic and secp256k1.

---
+++ vendor/elliptic/lib/elliptic.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var elliptic = exports;
+
+elliptic.version = '6.4.1';
+elliptic.utils = require('./elliptic/utils');
+elliptic.rand = require('../../brorand');
+elliptic.curve = require('./elliptic/curve');
+elliptic.curves = require('./elliptic/curves');
+
+// Protocols
+elliptic.ec = require('./elliptic/ec');
+elliptic.eddsa = require('./elliptic/eddsa');
---
+++ vendor/hash.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const SHA224 = require('../lib/sha224');
+const SHA256 = require('../lib/sha256');
+const SHA384 = require('../lib/sha384');
+const SHA512 = require('../lib/sha512');
+
+function toHash(Hash, hmacStrength, padLength) {
+  const BlockHash = function BlockHash() {
+    if (!(this instanceof BlockHash))
+      return new BlockHash();
+    this.ctx = new Hash();
+    this.ctx.init();
+  };
+
+  BlockHash.prototype.update = function update(data, enc) {
+    this.ctx.update(toBuffer(data, enc));
+    return this;
+  };
+
+  BlockHash.prototype.digest = function digest(enc) {
+    if (enc === 'hex')
+      return this.ctx.final().toString('hex');
+    return Array.prototype.slice.call(this.ctx.final());
+  };
+
+  BlockHash.bcrypto = Hash;
+  BlockHash.blockSize = Hash.blockSize * 8;
+  BlockHash.outSize = Hash.bits;
+  BlockHash.hmacStrength = hmacStrength;
+  BlockHash.padLength = padLength;
+
+  return BlockHash;
+}
+
+function toBuffer(buf, enc) {
+  if (!buf)
+    return Buffer.alloc(0);
+
+  if (typeof buf === 'string')
+    return Buffer.from(buf, enc || 'ascii');
+
+  if (Array.isArray(buf))
+    return Buffer.from(buf);
+
+  return buf;
+}
+
+exports.sha224 = toHash(SHA224, 192, 64);
+exports.sha256 = toHash(SHA256, 192, 64);
+exports.sha384 = toHash(SHA384, 192, 128);
+exports.sha512 = toHash(SHA512, 192, 128);
