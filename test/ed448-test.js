8840c3edda4c8cca28fd4aa22f65b66e65b4b5e2
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-11-10 05:50:47 -0800

    eddsa: rudimentary ed448 support.

---
+++ test/ed448-test.js
@@ -0,0 +1,112 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+const assert = require('./util/assert');
+const random = require('../lib/random');
+const ed448 = require('../lib/js/ed448');
+
+describe('Ed448', function() {
+  this.timeout(15000);
+
+  it('should generate keypair and sign', () => {
+    const msg = random.randomBytes(ed448.size);
+    const secret = ed448.privateKeyGenerate();
+    const pub = ed448.publicKeyCreate(secret);
+
+    assert(ed448.publicKeyVerify(pub));
+
+    const sig = ed448.sign(msg, secret);
+    assert(ed448.verify(msg, sig, pub));
+
+    sig[(Math.random() * sig.length) | 0] ^= 1;
+
+    assert(!ed448.verify(msg, sig, pub));
+
+    assert.bufferEqual(
+      ed448.privateKeyImport(ed448.privateKeyExport(secret)),
+      secret);
+
+    assert.bufferEqual(
+      ed448.privateKeyImportPKCS8(ed448.privateKeyExportPKCS8(secret)),
+      secret);
+
+    assert.bufferEqual(
+      ed448.publicKeyImport(ed448.publicKeyExport(pub)),
+      pub);
+
+    assert.bufferEqual(
+      ed448.publicKeyImportSPKI(ed448.publicKeyExportSPKI(pub)),
+      pub);
+  });
+
+  it('should do ECDH', () => {
+    const alicePriv = ed448.privateKeyGenerate();
+    const alicePub = ed448.publicKeyCreate(alicePriv);
+
+    const bobPriv = ed448.privateKeyGenerate();
+    const bobPub = ed448.publicKeyCreate(bobPriv);
+
+    const aliceSecret = ed448.derive(bobPub, alicePriv);
+    const bobSecret = ed448.derive(alicePub, bobPriv);
+
+    assert.bufferEqual(aliceSecret, bobSecret);
+  });
+
+  it('should convert to montgomery and back', () => {
+    const secret = ed448.privateKeyGenerate();
+    const pub = ed448.publicKeyCreate(secret);
+    const sign = (pub[56] & 0x80) !== 0;
+    const xpub = ed448.publicKeyConvert(pub);
+    const pub2 = ed448.publicKeyDeconvert(xpub, sign);
+
+    assert.bufferEqual(pub2, pub);
+  });
+
+  it('should sign and verify (vector)', () => {
+    const priv = Buffer.from(''
+      + 'd65df341ad13e008567688baedda8e9d'
+      + 'cdc17dc024974ea5b4227b6530e339bf'
+      + 'f21f99e68ca6968f3cca6dfe0fb9f4fa'
+      + 'b4fa135d5542ea3f01',
+      'hex');
+
+    const pub = Buffer.from(''
+      + 'df9705f58edbab802c7f8363cfe5560a'
+      + 'b1c6132c20a9f1dd163483a26f8ac53a'
+      + '39d6808bf4a1dfbd261b099bb03b3fb5'
+      + '0906cb28bd8a081f00',
+      'hex');
+
+    const msg = Buffer.from(''
+      + 'bd0f6a3747cd561bdddf4640a332461a'
+      + '4a30a12a434cd0bf40d766d9c6d458e5'
+      + '512204a30c17d1f50b5079631f64eb31'
+      + '12182da3005835461113718d1a5ef944',
+      'hex');
+
+    const sig = Buffer.from(''
+      + '554bc2480860b49eab8532d2a533b7d5'
+      + '78ef473eeb58c98bb2d0e1ce488a98b1'
+      + '8dfde9b9b90775e67f47d4a1c3482058'
+      + 'efc9f40d2ca033a0801b63d45b3b722e'
+      + 'f552bad3b4ccb667da350192b61c508c'
+      + 'f7b6b5adadc2c8d9a446ef003fb05cba'
+      + '5f30e88e36ec2703b349ca229c267083'
+      + '3900',
+      'hex');
+
+    const pub2 = ed448.publicKeyCreate(priv);
+
+    assert.bufferEqual(pub2, pub);
+
+    const sig2 = ed448.sign(msg, priv);
+
+    assert.bufferEqual(sig2, sig);
+
+    const result = ed448.verify(msg, sig, pub);
+
+    assert.strictEqual(result, true);
+  });
+});
