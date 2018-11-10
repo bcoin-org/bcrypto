c17b3b2e35016e5908c1d9545b58431d2eea53df
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-11-10 11:02:20 -0800

    src: minor.

--- src/bcrypto.h
+++ src/bcrypto.h
@@ -4,13 +4,6 @@
 #include <node.h>
 #include <nan.h>
 
-NAN_METHOD(pbkdf2);
-NAN_METHOD(pbkdf2_async);
-NAN_METHOD(scrypt);
-NAN_METHOD(scrypt_async);
 NAN_METHOD(cleanse);
-NAN_METHOD(encipher);
-NAN_METHOD(decipher);
-NAN_METHOD(random_fill);
 
 #endif
