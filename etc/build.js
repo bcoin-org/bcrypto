9f3ba44e30b7a41ba8035a66b72c60fe32bd4488
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-03-11 02:37:16 -0700

    etc: add build for elliptic.

---
+++ etc/build.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('bfile');
+const bpkg = require('bpkg');
+const {join, resolve} = require('path');
+
+const ROOT = resolve(__dirname);
+const INPUT = resolve(process.argv[2]);
+const DEPS = join(ROOT, 'deps');
+const PKG = join(ROOT, 'package');
+const MAIN = join(PKG, 'elliptic');
+const PKG_JSON = join(MAIN, 'package.json');
+const NM = join(MAIN, 'node_modules');
+const JS = join(ROOT, 'elliptic.js');
+const OUT = resolve(ROOT, '..', 'vendor', 'elliptic.js');
+
+const deps = [
+  [join(DEPS, 'bn.js'), join(NM, 'bn.js')],
+  [join(DEPS, 'brorand.js'), join(NM, 'brorand.js')],
+  [join(DEPS, 'hash.js'), join(NM, 'hash.js')],
+  [join(DEPS, 'hmac-drbg.js'), join(NM, 'hmac-drbg.js')],
+  [join(DEPS, 'inherits.js'), join(NM, 'inherits.js')]
+];
+
+// Complicated build.
+(async () => {
+  await fs.remove([PKG, JS]);
+
+  await bpkg({
+    input: INPUT,
+    output: PKG,
+    multi: true
+  });
+
+  const json = await fs.readJSON(PKG_JSON);
+
+  await fs.writeJSON(PKG_JSON, {
+    name: json.name,
+    version: json.version,
+    description: json.description,
+    homepage: json.homepage,
+    license: json.license,
+    main: json.main
+  });
+
+  for (const [from, to] of deps) {
+    await fs.remove(to);
+    await fs.copy(from, to);
+  }
+
+  await bpkg({
+    input: MAIN,
+    output: JS
+  });
+
+  let code = await fs.readFile(JS, 'utf8');
+
+  code = code.replace(/__require__/g, 'require');
+
+  await fs.writeFile(OUT, code);
+  await fs.remove(PKG);
+  await fs.remove(JS);
+})().catch((err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
---
+++ etc/deps/bn.js
@@ -0,0 +1,5 @@
+'use strict';
+
+/* global __require__ */
+
+module.exports = __require__('./bn.js');
