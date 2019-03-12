9b0cf70cfd14897eaa1e567c84fff542416a8685
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-03-12 15:16:07 -0700

    ssh: support multiple key types for new encoding.
    - fixes handshake-org/hs-airdrop#10
    - fixes bcoin-org/bcrypto#11

--- test/ssh-test.js
+++ test/ssh-test.js
@@ -13,16 +13,28 @@ const pubs = [
   Path.resolve(__dirname, 'data', 'id_dsa.pub'),
   Path.resolve(__dirname, 'data', 'id_rsa.pub'),
   Path.resolve(__dirname, 'data', 'id_ecdsa.pub'),
-  Path.resolve(__dirname, 'data', 'id_ed25519_unenc.pub'),
-  Path.resolve(__dirname, 'data', 'id_ed25519.pub')
+  Path.resolve(__dirname, 'data', 'id_dsa_modern.pub'),
+  Path.resolve(__dirname, 'data', 'id_dsa_modern_unenc.pub'),
+  Path.resolve(__dirname, 'data', 'id_rsa_modern.pub'),
+  Path.resolve(__dirname, 'data', 'id_rsa_modern_unenc.pub'),
+  Path.resolve(__dirname, 'data', 'id_ecdsa_modern.pub'),
+  Path.resolve(__dirname, 'data', 'id_ecdsa_modern_unenc.pub'),
+  Path.resolve(__dirname, 'data', 'id_ed25519.pub'),
+  Path.resolve(__dirname, 'data', 'id_ed25519_unenc.pub')
 ];
 
 const privs = [
   Path.resolve(__dirname, 'data', 'id_dsa'),
   Path.resolve(__dirname, 'data', 'id_rsa'),
   Path.resolve(__dirname, 'data', 'id_ecdsa'),
-  Path.resolve(__dirname, 'data', 'id_ed25519_unenc'),
-  Path.resolve(__dirname, 'data', 'id_ed25519')
+  Path.resolve(__dirname, 'data', 'id_dsa_modern'),
+  Path.resolve(__dirname, 'data', 'id_dsa_modern_unenc'),
+  Path.resolve(__dirname, 'data', 'id_rsa_modern'),
+  Path.resolve(__dirname, 'data', 'id_rsa_modern_unenc'),
+  Path.resolve(__dirname, 'data', 'id_ecdsa_modern'),
+  Path.resolve(__dirname, 'data', 'id_ecdsa_modern_unenc'),
+  Path.resolve(__dirname, 'data', 'id_ed25519'),
+  Path.resolve(__dirname, 'data', 'id_ed25519_unenc')
 ];
 
 const PASSPHRASE = '1234567890';
@@ -48,8 +60,13 @@ describe('SSH', function() {
   for (const file of privs) {
     const str = fs.readFileSync(file, 'utf8');
 
+    let passphrase = PASSPHRASE;
+
+    if (file.includes('modern'))
+      passphrase = 'foo';
+
     it('should deserialize and reserialize private keys', () => {
-      const key1 = SSHPrivateKey.fromString(str, PASSPHRASE);
+      const key1 = SSHPrivateKey.fromString(str, passphrase);
       const str1 = key1.toString();
       const key2 = SSHPrivateKey.fromString(str1);
       const str2 = key2.toString();
@@ -57,12 +74,12 @@ describe('SSH', function() {
       assert.deepStrictEqual(key1, key2);
       assert.strictEqual(str1, str2);
 
-      const str3 = key2.toString(PASSPHRASE);
-      const key3 = SSHPrivateKey.fromString(str3, PASSPHRASE);
+      const str3 = key2.toString(passphrase);
+      const key3 = SSHPrivateKey.fromString(str3, passphrase);
 
       let err;
       try {
-        SSHPrivateKey.fromString(str3, 'foo');
+        SSHPrivateKey.fromString(str3, 'bar');
       } catch (e) {
         err = e;
       }
