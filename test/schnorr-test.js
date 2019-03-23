841abb870bb7bd3f2c3531a2ce9096f7a2f90b97
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-03-23 15:52:59 -0700

    ecdsa: first pass at bip-schnorr.
    Not exposing this just yet.

---
+++ test/schnorr-test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const assert = require('bsert');
+const secp256k1 = require('../lib/js/secp256k1').ec;
+const csv = require('./util/csv');
+
+describe('Secp256k1+Schnorr', function() {
+  const iter = csv.asArray(`${__dirname}/data/schnorr-vectors.csv`);
+
+  for (const [key_, pub_, msg_, sig_, result_, comment_] of iter) {
+    const key = Buffer.from(key_, 'hex');
+    const pub = Buffer.from(pub_, 'hex');
+    const msg = Buffer.from(msg_, 'hex');
+    const sig = Buffer.from(sig_, 'hex');
+    const result = result_ === 'TRUE';
+    const comment = comment_ || `should verify ${sig_}`;
+
+    it(comment, () => {
+      if (key.length > 0) {
+        assert(secp256k1.privateKeyVerify(key));
+        assert.bufferEqual(secp256k1.publicKeyCreate(key), pub);
+        assert.bufferEqual(secp256k1.schnorrSign(msg, key), sig);
+      }
+
+      assert.strictEqual(secp256k1.schnorrVerify(msg, sig, pub), result);
+    });
+  }
+});
