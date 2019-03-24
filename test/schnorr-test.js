74ea9ba7df95769dfe8a65b5a8d579c4b464882c
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-03-23 19:21:55 -0700

    schnorr: batch verification.

--- test/schnorr-test.js
+++ test/schnorr-test.js
@@ -6,6 +6,8 @@ const csv = require('./util/csv');
 
 describe('Secp256k1+Schnorr', function() {
   const iter = csv.asArray(`${__dirname}/data/schnorr-vectors.csv`);
+  const valid = [];
+  const invalid = [];
 
   for (const [key_, pub_, msg_, sig_, result_, comment_] of iter) {
     const key = Buffer.from(key_, 'hex');
@@ -14,6 +16,12 @@ describe('Secp256k1+Schnorr', function() {
     const sig = Buffer.from(sig_, 'hex');
     const result = result_ === 'TRUE';
     const comment = comment_ || `should verify ${sig_}`;
+
+    (result ? valid : invalid).push({
+      message: msg,
+      signature: sig,
+      key: pub
+    });
 
     it(comment, () => {
       if (key.length > 0) {
@@ -25,4 +33,13 @@ describe('Secp256k1+Schnorr', function() {
       assert.strictEqual(secp256k1.schnorrVerify(msg, sig, pub), result);
     });
   }
+
+  it('should do batch verification', () => {
+    assert.strictEqual(secp256k1.schnorrBatchVerify(valid), true);
+
+    for (const item of invalid) {
+      assert.strictEqual(secp256k1.schnorrBatchVerify([item, ...valid]), false);
+      assert.strictEqual(secp256k1.schnorrBatchVerify([...valid, item]), false);
+    }
+  });
 });
