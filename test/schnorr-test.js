28d8ecacd83cc7d4db879c4e61f661541bab7584
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-04-28 00:07:59 -0700

    schnorr: clean up test vectors.

--- test/schnorr-test.js
+++ test/schnorr-test.js
@@ -2,29 +2,27 @@
 
 const assert = require('bsert');
 const secp256k1 = require('../lib/secp256k1');
-const csv = require('./util/csv');
+const vectors = require('./data/schnorr.json');
 
 describe('Secp256k1+Schnorr', function() {
-  const iter = csv.asArray(`${__dirname}/data/schnorr-vectors.csv`);
-  const vectors = [];
+  const parsed = [];
   const valid = [];
   const invalid = [];
 
   // Parse test vectors.
-  for (const [key_, pub_, msg_, sig_, result_, comment_] of iter) {
+  for (const [key_, pub_, msg_, sig_, result, comment_] of vectors) {
     const key = Buffer.from(key_, 'hex');
     const pub = Buffer.from(pub_, 'hex');
     const msg = Buffer.from(msg_, 'hex');
     const sig = Buffer.from(sig_, 'hex');
-    const result = result_ === 'TRUE';
     const comment = comment_ || `should verify ${sig_.toLowerCase()}`;
     const batch = result ? valid : invalid;
 
-    vectors.push([key, pub, msg, sig, result, comment]);
+    parsed.push([key, pub, msg, sig, result, comment]);
     batch.push([msg, sig, pub]);
   }
 
-  for (const [key, pub, msg, sig, result, comment] of vectors) {
+  for (const [key, pub, msg, sig, result, comment] of parsed) {
     it(comment, () => {
       if (key.length > 0) {
         assert(secp256k1.privateKeyVerify(key));
