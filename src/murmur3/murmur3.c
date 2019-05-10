b90eddcf59754fc72ee267f80cece420e8b60c73
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-05-10 03:42:18 -0700

    src/murmur3: refactor.

--- src/murmur3/murmur3.c
+++ src/murmur3/murmur3.c
@@ -77,12 +77,10 @@ bcrypto_murmur3_sum(const uint8_t *data, size_t len, uint32_t seed) {
 }
 
 uint32_t
-bcrypto_murmur3_tweak(
-  const uint8_t *data,
-  size_t len,
-  uint32_t n,
-  uint32_t tweak
-) {
+bcrypto_murmur3_tweak(const uint8_t *data,
+                      size_t len,
+                      uint32_t n,
+                      uint32_t tweak) {
   uint32_t seed = (n * 0xfba4c795ul) + tweak;
   return bcrypto_murmur3_sum(data, len, seed);
 }
