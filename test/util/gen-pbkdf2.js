b918cfb6b2fe5d9b45ce14fe54aeb1ba3e1c1b27
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-05-30 04:21:41 -0700

    test/pbkdf2: better test vectors.

--- test/util/gen-pbkdf2.js
+++ test/util/gen-pbkdf2.js
@@ -3,12 +3,21 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-function testVector() {
+const hashes = [
+  ['SHA1', 'sha1'],
+  ['SHA256', 'sha256'],
+  ['SHA512', 'sha512'],
+  ['SHA3_256', 'sha3-256'],
+  ['BLAKE2s256', 'blake2s256'],
+  ['BLAKE2b512', 'blake2b512']
+];
+
+function testVector(name) {
   const passwd = crypto.randomBytes(Math.random() * 64 | 0);
   const salt = crypto.randomBytes(Math.random() * 32 | 0);
-  const iter = crypto.randomRange(1, 5000);
-  const len = crypto.randomRange(1, 64);
-  const expect = crypto.pbkdf2Sync(passwd, salt, iter, len, 'sha256');
+  const iter = 1 + (Math.random() * 5000 | 0);
+  const len = 1 + (Math.random() * 64 | 0);
+  const expect = crypto.pbkdf2Sync(passwd, salt, iter, len, name);
 
   return {
     passwd,
@@ -18,18 +27,22 @@ function testVector() {
     expect
   };
 }
+
 const vectors = [];
 
-for (let i = 0; i < 50; i++) {
-  const {passwd, salt, iter, len, expect} = testVector();
+for (const [hash, name] of hashes) {
+  for (let i = 0; i < 10; i++) {
+    const {passwd, salt, iter, len, expect} = testVector(name);
 
-  vectors.push([
-    passwd.toString('hex'),
-    salt.toString('hex'),
-    iter,
-    len,
-    expect.toString('hex')
-  ]);
+    vectors.push([
+      hash,
+      passwd.toString('hex'),
+      salt.toString('hex'),
+      iter,
+      len,
+      expect.toString('hex')
+    ]);
+  }
 }
 
 fs.writeFileSync(`${__dirname}/../data/pbkdf2.json`,
