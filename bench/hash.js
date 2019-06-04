fde95aa45827e8f0bb00f9fd3c935e7b5cb4c454
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-04 08:43:54 -0700

    bench: add blake2s.

--- bench/hash.js
+++ bench/hash.js
@@ -3,6 +3,7 @@
 const bench = require('./bench');
 const sha256 = require('../lib/sha256');
 const blake2b = require('../lib/blake2b');
+const blake2s = require('../lib/blake2s');
 const sha3 = require('../lib/sha3');
 const random = require('../lib/random');
 
@@ -16,6 +17,10 @@ for (const size of [32, 64, 65, 512]) {
 
   bench(`blake2b (${size})`, rounds, () => {
     blake2b.digest(msg);
+  });
+
+  bench(`blake2s (${size})`, rounds, () => {
+    blake2s.digest(msg);
   });
 
   bench(`sha3 (${size})`, rounds, () => {
