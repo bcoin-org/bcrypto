ba010d90ca285dc25cb26d61c8bebdcf9aa15e25
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-04 17:00:17 -0700

    test: add more chachapoly tests. see #21.

---
+++ test/aead-test.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const assert = require('bsert');
+const AEAD = require('../lib/aead');
+const vectors = require('./data/aead.json');
+
+describe('AEAD (ChaCha20+Poly1305)', function() {
+  for (const [plain_, aad_, key_, nonce_, raw_, pk_] of vectors) {
+    const input = Buffer.from(plain_, 'hex');
+    const text = plain_.slice(0, 32) + '...';
+    const aad = Buffer.from(aad_, 'hex');
+    const key = Buffer.from(key_, 'hex');
+    const nonce = Buffer.from(nonce_, 'hex');
+    const raw = Buffer.from(raw_, 'hex');
+    const output = raw.slice(0, -16);
+    const tag = raw.slice(-16);
+    const pk = Buffer.from(pk_ || '', 'hex');
+
+    it(`should do encrypt and decrypt (${text})`, () => {
+      const data = Buffer.from(input);
+      const ctx = new AEAD();
+
+      ctx.init(key, nonce);
+
+      if (ctx.chacha20 && pk.length > 0)
+        assert.bufferEqual(ctx.polyKey, pk);
+
+      ctx.aad(aad);
+      ctx.encrypt(data);
+
+      assert.bufferEqual(data, output);
+      assert.bufferEqual(ctx.final(), tag);
+
+      ctx.init(key, nonce);
+
+      if (ctx.chacha20 && pk.length > 0)
+        assert.bufferEqual(ctx.polyKey, pk);
+
+      ctx.aad(aad);
+      ctx.decrypt(data);
+
+      assert.bufferEqual(data, input);
+      assert.bufferEqual(ctx.final(), tag);
+    });
+
+    it(`should do one-shot encrypt and decrypt (${text})`, () => {
+      const data = Buffer.from(input);
+      const mac = AEAD.encrypt(key, nonce, data, aad);
+
+      assert.bufferEqual(data, output);
+      assert.bufferEqual(mac, tag);
+
+      assert(AEAD.decrypt(key, nonce, data, tag, aad));
+      assert.bufferEqual(data, input);
+
+      AEAD.encrypt(key, nonce, data, aad);
+
+      key[0] ^= 1;
+
+      assert(!AEAD.decrypt(key, nonce, Buffer.from(data), tag, key));
+
+      key[0] ^= 1;
+
+      assert(AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+
+      nonce[0] ^= 1;
+
+      assert(!AEAD.decrypt(key, nonce, Buffer.from(data), tag, key));
+
+      nonce[0] ^= 1;
+
+      assert(AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+
+      tag[0] ^= 1;
+
+      assert(!AEAD.decrypt(key, nonce, Buffer.from(data), tag, key));
+
+      tag[0] ^= 1;
+
+      assert(AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+
+      if (data.length > 0) {
+        data[0] ^= 1;
+
+        assert(!AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+
+        data[0] ^= 1;
+
+        assert(AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+      }
+
+      if (aad.length > 0) {
+        aad[0] ^= 1;
+
+        assert(!AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+
+        aad[0] ^= 1;
+
+        assert(AEAD.decrypt(key, nonce, Buffer.from(data), tag, aad));
+      }
+    });
+  }
+});
---
+++ test/poly1305-test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const assert = require('bsert');
+const Poly1305 = require('../lib/poly1305');
+
+describe('Poly1305', function() {
+  it('should perform poly1305 (1)', () => {
+    const key = Buffer.allocUnsafe(32);
+    const msg = Buffer.allocUnsafe(73);
+    const tag = Buffer.from('ddb9da7ddd5e52792730ed5cda5f90a4', 'hex');
+
+    for (let i = 0; i < key.length; i++)
+      key[i] = i + 221;
+
+    for (let i = 0; i < msg.length; i++)
+      msg[i] = i + 121;
+
+    const mac = Poly1305.auth(msg, key);
+
+    assert(Poly1305.verify(mac, tag));
+    assert.bufferEqual(mac, tag);
+
+    mac[0] ^= 1;
+
+    assert(!Poly1305.verify(mac, tag));
+  });
+
+  it('should perform poly1305 (2)', () => {
+    const key = Buffer.from('85d6be7857556d337f4452fe42d506a'
+                          + '80103808afb0db2fd4abff6af4149f51b', 'hex');
+    const msg = Buffer.from('Cryptographic Forum Research Group', 'ascii');
+    const tag = Buffer.from('a8061dc1305136c6c22b8baf0c0127a9', 'hex');
+    const mac = Poly1305.auth(msg, key);
+
+    assert(Poly1305.verify(mac, tag));
+
+    mac[0] ^= 1;
+
+    assert(!Poly1305.verify(mac, tag));
+  });
+});
