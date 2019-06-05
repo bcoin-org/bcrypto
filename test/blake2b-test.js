2a9177b37b15e4dffc3dd14d5293555b2c4d41bb
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-04 23:45:47 -0700

    test: minor.

--- test/blake2b-test.js
+++ test/blake2b-test.js
@@ -1,37 +1,33 @@
 'use strict';
 
 const assert = require('bsert');
-const {BLAKE2b} = require('../');
+const BLAKE2b = require('../lib/blake2b');
 const vectors = require('./data/blake2b.json');
-
-function testHash(msg, size = 64, key = '', expect) {
-  const m = Buffer.from(msg, 'hex');
-  const k = Buffer.from(key, 'hex');
-  const e = Buffer.from(expect, 'hex');
-
-  const hash = BLAKE2b.digest(m, size, k);
-
-  assert.bufferEqual(hash, e);
-
-  const ctx = new BLAKE2b();
-  ctx.init(size, k);
-
-  const ch = Buffer.allocUnsafe(1);
-
-  for (let i = 0; i < m.length; i++) {
-    ch[0] = m[i];
-    ctx.update(ch);
-  }
-
-  assert.bufferEqual(ctx.final(), e);
-}
 
 describe('BLAKE2b', function() {
   for (const [msg, size, key, expect] of vectors) {
     const text = expect.slice(0, 32) + '...';
 
     it(`should get BLAKE2b hash of ${text}`, () => {
-      testHash(msg, size, key, expect);
+      const m = Buffer.from(msg, 'hex');
+      const k = Buffer.from(key, 'hex');
+      const e = Buffer.from(expect, 'hex');
+
+      const hash = BLAKE2b.digest(m, size, k);
+
+      assert.bufferEqual(hash, e);
+
+      const ctx = new BLAKE2b();
+      ctx.init(size, k);
+
+      const ch = Buffer.allocUnsafe(1);
+
+      for (let i = 0; i < m.length; i++) {
+        ch[0] = m[i];
+        ctx.update(ch);
+      }
+
+      assert.bufferEqual(ctx.final(), e);
     });
   }
 });
