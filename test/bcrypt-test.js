f83b569e0f6d87e3fee6efc7912a112d4140f3b0
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-05 02:16:08 -0700

    eb2k/bcrypt: more test vectors.

--- test/bcrypt-test.js
+++ test/bcrypt-test.js
@@ -2,27 +2,71 @@
 
 const assert = require('bsert');
 const bcrypt = require('../lib/bcrypt');
+const hash256vectors = require('./data/bcrypt-hash256.json');
 
-const test = {
-  pass: '1234567890',
-  salt: 'edf2ee939723f10f09cca07c90b31c47',
-  rounds: 16,
-  size: 48,
-  key: '8476c2efc085d68616acf2809661839427346028dfc98ae0e82'
-    + '584c72fdfbc337920678fdb35c7296de17dfeb4f988f5'
-};
+const vectors = [
+  [
+    'foo',
+    'd8d5105271003f18afb751584ac9df4d',
+    16,
+    48,
+    'ef312085086a7846ef9d43644ca3361d267622efff8e5cebed1ea9513a6aa0b5160de8bf76112e0c0edec68594777f75'
+  ],
+  [
+    'foo',
+    'eaf595e9bd3b2a8f1cfa249c7d8004c3',
+    16,
+    48,
+    '44f364368852323144945ad404289b52c02d661817a321c1cd13b36041be584020a028bdfe138520c57f14d34d03fa62'
+  ],
+  [
+    'foo',
+    '9236ca8ddac428f6c83f6bb74ca5558e',
+    16,
+    48,
+    '87700d9f89afab94b564ebbe7f85de7a2eb2aacbe3a423d1d4bbe290a9a2fd2e4e4dd1644029917a6db6871235a54c41'
+  ],
+  [
+    '1234567890',
+    'edf2ee939723f10f09cca07c90b31c47',
+    16,
+    48,
+    '8476c2efc085d68616acf2809661839427346028dfc98ae0e82584c72fdfbc337920678fdb35c7296de17dfeb4f988f5'
+  ]
+];
 
 describe('Bcrypt', function() {
   this.timeout(10000);
 
-  it('should derive key', () => {
-    const key = bcrypt.pbkdf(
-      Buffer.from(test.pass, 'binary'),
-      Buffer.from(test.salt, 'hex'),
-      test.rounds,
-      test.size
-    );
+  describe('Hash192', () => {
+    it('should derive key (hash192)');
+  });
 
-    assert.strictEqual(key.toString('hex'), test.key);
+  describe('Hash256', () => {
+    for (const [pass_, salt_, rounds, expect_] of hash256vectors) {
+      const pass = Buffer.from(pass_, 'hex');
+      const salt = Buffer.from(salt_, 'hex');
+      const expect = Buffer.from(expect_, 'hex');
+      const text = expect_.slice(0, 32) + '...';
+
+      it(`should derive key (hash256): ${text}`, () => {
+        const key = bcrypt.hash256(pass, salt, rounds);
+        assert.bufferEqual(key, expect);
+      });
+    }
+  });
+
+  describe('PBKDF', () => {
+    for (const [pass_, salt_, rounds, size, expect_] of vectors) {
+      const pass = Buffer.from(pass_, 'binary');
+      const salt = Buffer.from(salt_, 'hex');
+      const expect = Buffer.from(expect_, 'hex');
+      const text = expect_.slice(0, 32) + '...';
+
+      it(`should derive key (pbkdf): ${text}`, () => {
+        const key = bcrypt.pbkdf(pass, salt, rounds, size);
+        assert.bufferEqual(key, expect);
+      });
+    }
   });
 });
--- test/eb2k-test.js
+++ test/eb2k-test.js
@@ -4,92 +4,92 @@ const assert = require('bsert');
 const eb2k = require('../lib/eb2k');
 
 const vectors = [
-  {
-    pass: '1234567890',
-    salt: '286e37f0b031c0d2c72c674b116e5342',
-    keyLen: 16,
-    ivLen: 16,
-    key: '1fc6b89794d36b3ce33087de2c107fb6',
-    iv: '8df8d21d44bfe94ab64cde7d6496857f'
-  },
-  {
-    pass: '1234567890',
-    salt: '113bdee3c90ee160997b2e373660368e',
-    keyLen: 16,
-    ivLen: 16,
-    key: '85560be2bd49c607ca036024999c3a30',
-    iv: 'daddbffe81ee02330c98d492c8f430bd'
-  },
-  {
-    pass: 'foo',
-    salt: '113bdee3c90ee160997b2e373660368e',
-    keyLen: 16,
-    ivLen: 16,
-    key: 'b25272c2fa948e94d01242e297762876',
-    iv: '562dedfd27b08b383254ffe1efc7b60d'
-  },
-  {
-    pass: '1234567890',
-    salt: 'bdb867fd73611c0586852f406b934243',
-    keyLen: 16,
-    ivLen: 16,
-    key: '85dad080e8a280f6f9ef18808afea519',
-    iv: 'a3ada9edf9fb2c4433a6efbbc210505a'
-  },
-  {
-    pass: '1234567890',
-    salt: '0eca5c2e3a32fa90e57d685621eb9786',
-    keyLen: 16,
-    ivLen: 16,
-    key: '0a9b842e6bbf13a26a5e99e5ec625857',
-    iv: 'c48bd7cfd5803964a8132326692ef6d8'
-  },
-  {
-    pass: 'foo',
-    salt: '0eca5c2e3a32fa90e57d685621eb9786',
-    keyLen: 16,
-    ivLen: 16,
-    key: 'df157b6c6d652f934a5fee158c5cc0e7',
-    iv: '9e434c07188328c657638b42b9b2040d'
-  },
-  {
-    pass: '1234567890',
-    salt: 'f27df2f6f6856e9bb4e2fe4c5269162a',
-    keyLen: 16,
-    ivLen: 16,
-    key: '654a6043e4e021375d0954f3b1a66fb4',
-    iv: '361d6661e43465d2ae203380f0a30c74'
-  },
-  {
-    pass: '1234567890',
-    salt: 'acf9985e9f755604b48dc03f48c8f584',
-    keyLen: 16,
-    ivLen: 16,
-    key: '7b8b628d62ca33fa5133bed68e5d18b9',
-    iv: '218e80364ecac6bf3fee95781baa98e6'
-  },
-  {
-    pass: 'foo',
-    salt: 'acf9985e9f755604b48dc03f48c8f584',
-    keyLen: 16,
-    ivLen: 16,
-    key: '040b57f89272efc3e225672799a9d2b7',
-    iv: '7b611924696718c6fbfb600965ee587e'
-  }
+  [
+    '1234567890',
+    '286e37f0b031c0d2c72c674b116e5342',
+    16,
+    16,
+    '1fc6b89794d36b3ce33087de2c107fb6',
+    '8df8d21d44bfe94ab64cde7d6496857f'
+  ],
+  [
+    '1234567890',
+    '113bdee3c90ee160997b2e373660368e',
+    16,
+    16,
+    '85560be2bd49c607ca036024999c3a30',
+    'daddbffe81ee02330c98d492c8f430bd'
+  ],
+  [
+    'foo',
+    '113bdee3c90ee160997b2e373660368e',
+    16,
+    16,
+    'b25272c2fa948e94d01242e297762876',
+    '562dedfd27b08b383254ffe1efc7b60d'
+  ],
+  [
+    '1234567890',
+    'bdb867fd73611c0586852f406b934243',
+    16,
+    16,
+    '85dad080e8a280f6f9ef18808afea519',
+    'a3ada9edf9fb2c4433a6efbbc210505a'
+  ],
+  [
+    '1234567890',
+    '0eca5c2e3a32fa90e57d685621eb9786',
+    16,
+    16,
+    '0a9b842e6bbf13a26a5e99e5ec625857',
+    'c48bd7cfd5803964a8132326692ef6d8'
+  ],
+  [
+    'foo',
+    '0eca5c2e3a32fa90e57d685621eb9786',
+    16,
+    16,
+    'df157b6c6d652f934a5fee158c5cc0e7',
+    '9e434c07188328c657638b42b9b2040d'
+  ],
+  [
+    '1234567890',
+    'f27df2f6f6856e9bb4e2fe4c5269162a',
+    16,
+    16,
+    '654a6043e4e021375d0954f3b1a66fb4',
+    '361d6661e43465d2ae203380f0a30c74'
+  ],
+  [
+    '1234567890',
+    'acf9985e9f755604b48dc03f48c8f584',
+    16,
+    16,
+    '7b8b628d62ca33fa5133bed68e5d18b9',
+    '218e80364ecac6bf3fee95781baa98e6'
+  ],
+  [
+    'foo',
+    'acf9985e9f755604b48dc03f48c8f584',
+    16,
+    16,
+    '040b57f89272efc3e225672799a9d2b7',
+    '7b611924696718c6fbfb600965ee587e'
+  ]
 ];
 
 describe('EB2K', function() {
-  for (const test of vectors) {
-    it(`should derive key ${test.key}`, () => {
-      const [key, iv] = eb2k.derive(
-        Buffer.from(test.pass, 'binary'),
-        Buffer.from(test.salt, 'hex'),
-        test.keyLen,
-        test.ivLen
-      );
+  for (const [pass_, salt_, keyLen, ivLen, key_, iv_] of vectors) {
+    const pass = Buffer.from(pass_, 'binary');
+    const salt = Buffer.from(salt_, 'hex');
+    const keyOut = Buffer.from(key_, 'hex');
+    const ivOut = Buffer.from(iv_, 'hex');
 
-      assert.strictEqual(key.toString('hex'), test.key);
-      assert.strictEqual(iv.toString('hex'), test.iv);
+    it(`should derive key ${key_}`, () => {
+      const [key, iv] = eb2k.derive(pass, salt, keyLen, ivLen);
+
+      assert.bufferEqual(key, keyOut);
+      assert.bufferEqual(iv, ivOut);
     });
   }
 });
