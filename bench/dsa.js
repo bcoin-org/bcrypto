20c37857e478a76555522bf15a1798f75ff70983
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-06 01:50:39 -0700

    bench/dsa: use 1024 bit keys.

--- bench/dsa.js
+++ bench/dsa.js
@@ -7,7 +7,7 @@ const mul = dsa.native ? 10 : 1;
 
 {
   const rounds = 100 * mul;
-  const key = dsa.privateKeyGenerate();
+  const key = dsa.privateKeyGenerate(1024);
   const pub = dsa.publicKeyCreate(key);
   const msg = random.randomBytes(32);
   const sig = dsa.sign(msg, key);
