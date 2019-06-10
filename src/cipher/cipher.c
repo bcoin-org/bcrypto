0b3a9ef18a077178509cca2a9f4dae447cf493c5
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-10 03:49:28 -0700

    src/cipher: fix ofb xor.

--- src/cipher/cipher.c
+++ src/cipher/cipher.c
@@ -399,7 +399,7 @@ bcrypto_cipher_crypt(bcrypto_cipher_t *cipher, uint8_t *dst,
 
       for (size_t i = 0; i < length; i += block_size) {
         fn(cipher->ctx, block_size, state, state);
-        memxor3(&dst[i * block_size], &src[i * block_size], state, block_size);
+        memxor3(&dst[i], &src[i], state, block_size);
       }
 
       break;
