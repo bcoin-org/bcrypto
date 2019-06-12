746eacdfa622999d1eef5e6a51b27c83532f8a8d
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-06-11 19:18:25 -0700

    bcrypto: remove legacy drbg.

--- test/bcrypto-test.js
+++ test/bcrypto-test.js
@@ -44,7 +44,6 @@ describe('Bcrypto', function() {
         assert.strictEqual(bcrypto.CSHAKE128.native, 0);
         assert.strictEqual(bcrypto.CSHAKE256.native, 0);
         assert.strictEqual(bcrypto.CtrDRBG.native, 0);
-        assert.strictEqual(bcrypto.DRBG.native, 0);
         assert.strictEqual(bcrypto.dsa.native, 0);
         assert.strictEqual(bcrypto.dsaies.native, undefined);
         assert.strictEqual(bcrypto.eb2k.native, 0);
@@ -140,7 +139,6 @@ describe('Bcrypto', function() {
         assert.strictEqual(bcrypto.CSHAKE128.native, 0);
         assert.strictEqual(bcrypto.CSHAKE256.native, 0);
         assert.strictEqual(bcrypto.CtrDRBG.native, 0);
-        assert.strictEqual(bcrypto.DRBG.native, 0);
         assert.strictEqual(bcrypto.dsa.native, 1);
         assert.strictEqual(bcrypto.dsaies.native, undefined);
         assert.strictEqual(bcrypto.eb2k.native, 0);
@@ -238,7 +236,6 @@ describe('Bcrypto', function() {
         assert.strictEqual(bcrypto.CSHAKE128.native, 2);
         assert.strictEqual(bcrypto.CSHAKE256.native, 2);
         assert.strictEqual(bcrypto.CtrDRBG.native, 0);
-        assert.strictEqual(bcrypto.DRBG.native, 0);
         assert.strictEqual(bcrypto.dsa.native, NODE_MAJOR >= 10 ? 2 : 1);
         assert.strictEqual(bcrypto.dsaies.native, undefined);
         assert.strictEqual(bcrypto.eb2k.native, 0);
