6fdd99e5d113e57e47928937294c1fd7802d6534
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-07-10 13:45:04 -0700

    bench/bn: more benchmarks.

--- bench/bench.js
+++ bench/bench.js
@@ -1,6 +1,20 @@
 'use strict';
 
+const f = process.argv.indexOf('-f');
+const g = process.argv.indexOf('-g');
+const b = process.argv.indexOf('-B');
+const i = f !== -1 ? f : g;
+const grep = i !== -1 && i + 1 < process.argv.length
+  ? new RegExp(process.argv[i + 1])
+  : /^/;
+
+if (b !== -1 && b + 1 < process.argv.length)
+  process.env.NODE_BACKEND = process.argv[b + 1];
+
 module.exports = function bench(name, ops, cb) {
+  if (!grep.test(name))
+    return;
+
   const start = process.hrtime();
 
   for (let i = 0; i < ops; i++)
