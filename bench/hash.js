268eef38959eb947779b12822c84baf9491b3a57
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-07-30 14:42:01 -0700

    bench/hash: update benchmarks.

--- bench/hash.js
+++ bench/hash.js
@@ -2,17 +2,27 @@
 
 const bench = require('./bench');
 const sha256 = require('../lib/sha256');
+const sha512 = require('../lib/sha512');
+const ripemd160 = require('../lib/ripemd160');
 const blake2b = require('../lib/blake2b');
 const blake2s = require('../lib/blake2s');
 const sha3 = require('../lib/sha3');
 const random = require('../lib/random');
 
-for (const size of [32, 64, 65, 512]) {
+for (const size of [32, 64, 65, 128, 512]) {
   const rounds = 200000;
   const msg = random.randomBytes(size);
 
   bench(`sha256 (${size})`, rounds, () => {
     sha256.digest(msg);
+  });
+
+  bench(`sha512 (${size})`, rounds, () => {
+    sha512.digest(msg);
+  });
+
+  bench(`ripemd160 (${size})`, rounds, () => {
+    ripemd160.digest(msg);
   });
 
   bench(`blake2b (${size})`, rounds, () => {
