b5a1c55921db56cc35286c32f464cb5a9fc5eea1
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-08-21 14:46:05 -0700

    src/ed25519: remove unused constant.

--- src/ed25519/curve25519-donna-32bit.h
+++ src/ed25519/curve25519-donna-32bit.h
@@ -11,13 +11,6 @@ typedef uint32_t bignum25519align16[12];
 static const uint32_t reduce_mask_25 = (1 << 25) - 1;
 static const uint32_t reduce_mask_26 = (1 << 26) - 1;
 
-/* 1 / 2 */
-static const bignum25519 curve25519_i2 = {
-  0x3fffff7, 0x1ffffff, 0x3ffffff, 0x1ffffff,
-  0x3ffffff, 0x1ffffff, 0x3ffffff, 0x1ffffff,
-  0x3ffffff, 0x0ffffff
-};
-
 /* out = in */
 DONNA_INLINE static void
 curve25519_copy(bignum25519 out, const bignum25519 in) {
