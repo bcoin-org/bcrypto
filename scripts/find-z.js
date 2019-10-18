53d536bc1c63d6d40332691eb48b28723447c3b9
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-10-18 03:38:29 -0700

    scripts/find-z: minor.

--- scripts/find-z.js
+++ scripts/find-z.js
@@ -35,7 +35,7 @@ function printZ(curve) {
   } else if (curve.type === 'mont' || curve.type === 'edwards') {
     alg = 'elligator2';
 
-    if (ell1 && curve.type === 'edwards') {
+    if (ell1 && curve.type === 'edwards' && !curve.twisted) {
       if (curve.p.andln(3) === 3 && curve.d.redJacobi() === -1)
         alg = 'elligator1';
     }
