e03ee9a25d716f20501a999d3581a18938e6eca3
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-11-12 13:42:40 -0800

    bench/svdw: minor.

--- bench/svdw.js
+++ bench/svdw.js
@@ -10,7 +10,7 @@ const mul = secp256k1.native ? 10 : 1;
   const key = Buffer.from('2576e04eda9e90aa0be40121a5b19612'
                         + '08ec8573c9bb02f75a5f69c9b63525b7', 'hex');
 
-  const pub = secp256k1.publicKeyCreate(key);
+  const pub = secp256k1.publicKeyCreate(key, false);
 
   const tweak = Buffer.from('31260986ee940fa71d2c4cc7c00d4b1e'
                           + 'c2131b24f2b6243f48c2cbd3b7b82ea3', 'hex');
