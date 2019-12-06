66097bedb7d80b45c7fd4327ce4d6da7ee00dfb9
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-12-06 05:44:28 -0800

    elliptic: flip inverse naming convention.

--- test/elliptic-test.js
+++ test/elliptic-test.js
@@ -2784,24 +2784,24 @@ describe('Elliptic', function() {
       {
         // c = 2 / s^2
         // r = c + 1 / c
-        const {s, is, i2, two} = curve;
+        const {s, si, i2, two} = curve;
         const c = two.redDiv(s.redSqr());
-        const ic = c.redInvert();
+        const ci = c.redInvert();
         const r = c.redAdd(c.redInvert());
-        const ic2 = c.redSqr().redInvert();
+        const ci2 = c.redSqr().redInvert();
 
         // c = 2 / s^2 = 2 * (1 / s)^2
         // 1 / c = s^2 / 2
         // 1 / c^2 = (1 / c)^2
-        const c_ = is.redSqr().redIMuln(2);
-        const ic_ = s.redSqr().redMul(i2);
-        const r_ = c_.redAdd(ic_);
-        const ic2_ = ic_.redSqr();
+        const c_ = si.redSqr().redIMuln(2);
+        const ci_ = s.redSqr().redMul(i2);
+        const r_ = c_.redAdd(ci_);
+        const ci2_ = ci_.redSqr();
 
         assert(c_.eq(c));
-        assert(ic_.eq(ic));
+        assert(ci_.eq(ci));
         assert(r_.eq(r));
-        assert(ic2_.eq(ic2));
+        assert(ci2_.eq(ci2));
       }
 
       const r1 = curve.field(1337);
@@ -3001,7 +3001,7 @@ describe('Elliptic', function() {
 
       {
         // -b * (1 / a) == -b / a
-        const a = curve.b.redNeg().redMul(curve.ia);
+        const a = curve.b.redNeg().redMul(curve.ai);
         const b = curve.b.redNeg().redDiv(curve.a);
 
         assert(a.eq(b));
@@ -3009,7 +3009,7 @@ describe('Elliptic', function() {
 
       {
         // b * (1 / z) * (1 / a) == b / (z * a)
-        const a = curve.b.redMul(curve.iz).redMul(curve.ia);
+        const a = curve.b.redMul(curve.zi).redMul(curve.ai);
         const b = curve.b.redDiv(curve.z.redMul(curve.a));
 
         assert(a.eq(b));
@@ -3090,7 +3090,7 @@ describe('Elliptic', function() {
 
       {
         // (1 / 3) * (1 / z)^2 == 1 / (3 * z^2)
-        const a = curve.i3.redMul(curve.iz.redSqr());
+        const a = curve.i3.redMul(curve.zi.redSqr());
         const b = curve.z.redSqr().redIMuln(3).redInvert();
 
         assert(a.eq(b));
@@ -3099,8 +3099,8 @@ describe('Elliptic', function() {
       {
         // (1 / 3) * (1 / z)^2 == 1 / (3 * z^2)
         const z = curve.randomField(rng);
-        const iz = z.redInvert();
-        const a = curve.i3.redMul(iz.redSqr());
+        const zi = z.redInvert();
+        const a = curve.i3.redMul(zi.redSqr());
         const b = z.redSqr().redIMuln(3).redInvert();
 
         assert(a.eq(b));
@@ -3283,7 +3283,7 @@ describe('Elliptic', function() {
       // Satisfies jacobi(z) == -1
       // and jacobi(g(b / (z * a))) == 1.
       wei.z = wei.field(-3);
-      wei.iz = wei.z.redInvert();
+      wei.zi = wei.z.redInvert();
 
       assert(!wei.z.redIsSquare());
       assert(wei.solveY2(wei.b.redMul(zai)).redIsSquare());
