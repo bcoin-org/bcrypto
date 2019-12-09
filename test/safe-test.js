d93f5ccb5ef336cd252960254d5b2fcdec24d3ce
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-12-09 06:24:57 -0800

    safe: parameter consistency.

--- test/safe-test.js
+++ test/safe-test.js
@@ -61,50 +61,94 @@ describe('Safe', function() {
   });
 
   it('should select int', () => {
-    assert.strictEqual(safe.safeSelect(0, 1, 2), 2);
-    assert.strictEqual(safe.safeSelect(1, 1, 2), 1);
+    assert.strictEqual(safe.safeSelect(-1, -2, 0), -1);
+    assert.strictEqual(safe.safeSelect(-1, -2, 1), -2);
+    assert.strictEqual(safe.safeSelect(0, 0, 0), 0);
+    assert.strictEqual(safe.safeSelect(0, 0, 1), 0);
+    assert.strictEqual(safe.safeSelect(1, 2, 0), 1);
+    assert.strictEqual(safe.safeSelect(1, 2, 1), 2);
+    assert.strictEqual(safe.safeSelect(-100, 100, 0), -100);
+    assert.strictEqual(safe.safeSelect(-100, 100, 1), 100);
   });
 
   it('should compare int (LT)', () => {
+    assert.strictEqual(safe.safeLT(-1, -2), 0);
+    assert.strictEqual(safe.safeLT(-2, -2), 0);
+    assert.strictEqual(safe.safeLT(-3, -2), 1);
+    assert.strictEqual(safe.safeLT(0, 0), 0);
     assert.strictEqual(safe.safeLT(1, 2), 1);
     assert.strictEqual(safe.safeLT(2, 2), 0);
     assert.strictEqual(safe.safeLT(3, 2), 0);
+    assert.strictEqual(safe.safeLT(-100, 100), 1);
+    assert.strictEqual(safe.safeLT(100, -100), 0);
   });
 
   it('should compare int (LTE)', () => {
+    assert.strictEqual(safe.safeLTE(-1, -2), 0);
+    assert.strictEqual(safe.safeLTE(-2, -2), 1);
+    assert.strictEqual(safe.safeLTE(-3, -2), 1);
+    assert.strictEqual(safe.safeLTE(0, 0), 1);
     assert.strictEqual(safe.safeLTE(1, 2), 1);
     assert.strictEqual(safe.safeLTE(2, 2), 1);
     assert.strictEqual(safe.safeLTE(3, 2), 0);
+    assert.strictEqual(safe.safeLTE(-100, 100), 1);
+    assert.strictEqual(safe.safeLTE(100, -100), 0);
   });
 
   it('should compare int (GT)', () => {
+    assert.strictEqual(safe.safeGT(-1, -2), 1);
+    assert.strictEqual(safe.safeGT(-2, -2), 0);
+    assert.strictEqual(safe.safeGT(-3, -2), 0);
+    assert.strictEqual(safe.safeGT(0, 0), 0);
     assert.strictEqual(safe.safeGT(1, 2), 0);
     assert.strictEqual(safe.safeGT(2, 2), 0);
     assert.strictEqual(safe.safeGT(3, 2), 1);
+    assert.strictEqual(safe.safeGT(-100, 100), 0);
+    assert.strictEqual(safe.safeGT(100, -100), 1);
   });
 
   it('should compare int (GTE)', () => {
+    assert.strictEqual(safe.safeGTE(-1, -2), 1);
+    assert.strictEqual(safe.safeGTE(-2, -2), 1);
+    assert.strictEqual(safe.safeGTE(-3, -2), 0);
+    assert.strictEqual(safe.safeGTE(0, 0), 1);
     assert.strictEqual(safe.safeGTE(1, 2), 0);
     assert.strictEqual(safe.safeGTE(2, 2), 1);
     assert.strictEqual(safe.safeGTE(3, 2), 1);
+    assert.strictEqual(safe.safeGTE(-100, 100), 0);
+    assert.strictEqual(safe.safeGTE(100, -100), 1);
   });
 
   it('should take min', () => {
+    assert.strictEqual(safe.safeMin(-1, -2), -2);
+    assert.strictEqual(safe.safeMin(-2, -1), -2);
+    assert.strictEqual(safe.safeMin(-2, -2), -2);
+    assert.strictEqual(safe.safeMin(0, 0), 0);
     assert.strictEqual(safe.safeMin(1, 2), 1);
     assert.strictEqual(safe.safeMin(2, 1), 1);
     assert.strictEqual(safe.safeMin(2, 2), 2);
+    assert.strictEqual(safe.safeMin(-100, 100), -100);
+    assert.strictEqual(safe.safeMin(100, -100), -100);
   });
 
   it('should take max', () => {
+    assert.strictEqual(safe.safeMax(-1, -2), -1);
+    assert.strictEqual(safe.safeMax(-2, -1), -1);
+    assert.strictEqual(safe.safeMax(-2, -2), -2);
+    assert.strictEqual(safe.safeMax(0, 0), 0);
     assert.strictEqual(safe.safeMax(1, 2), 2);
     assert.strictEqual(safe.safeMax(2, 1), 2);
     assert.strictEqual(safe.safeMax(2, 2), 2);
+    assert.strictEqual(safe.safeMax(-100, 100), 100);
+    assert.strictEqual(safe.safeMax(100, -100), 100);
   });
 
   it('should take abs', () => {
+    assert.strictEqual(safe.safeAbs(-100), 100);
     assert.strictEqual(safe.safeAbs(-1), 1);
     assert.strictEqual(safe.safeAbs(0), 0);
     assert.strictEqual(safe.safeAbs(1), 1);
+    assert.strictEqual(safe.safeAbs(100), 100);
   });
 
   it('should take bool', () => {
@@ -124,10 +168,10 @@ describe('Safe', function() {
     const out = Buffer.alloc(6, 0x00);
     const foo = Buffer.from('foobar');
 
-    safe.safeCopy(0, out, foo);
+    safe.safeCopy(out, foo, 0);
     assert(out.equals(zero));
 
-    safe.safeCopy(1, out, foo);
+    safe.safeCopy(out, foo, 1);
     assert(out.equals(foo));
   });
 });
